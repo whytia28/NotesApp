7fc68a450434ac3b00132f9555d3c407845661b9
whytia28 <wahyu.sp9@gmail.com>
2020-07-11 20:28:27 +0700

    implementasi content provider

---
+++ consumerapp/src/main/java/com/example/consumerapp/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.consumerapp
+
+import android.content.Intent
+import android.database.ContentObserver
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.consumerapp.adapter.NoteAdapter
+import com.example.consumerapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.example.consumerapp.entity.Note
+import com.example.consumerapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: NoteAdapter
+
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Consumer Notes"
+
+        rv_notes.layoutManager = LinearLayoutManager(this)
+        rv_notes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        rv_notes.adapter = adapter
+
+        fab_add.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+        }
+
+        val handlerThread = HandlerThread("DataObserver")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+
+        val myObserver = object : ContentObserver(handler) {
+            override fun onChange(selfChange: Boolean) {
+                loadNotesAsync()
+            }
+        }
+
+        contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+
+        if (savedInstanceState == null) {
+            // proses ambil data
+            loadNotesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+    }
+
+    private fun loadNotesAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            progress_bar.visibility = View.VISIBLE
+            val deferredNotes = async(Dispatchers.IO) {
+
+                // CONTENT_URI = content://com.example.mynotesapp/noteCursor
+                val cursor = contentResolver?.query(CONTENT_URI, null, null, null, null)
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+            val notes = deferredNotes.await()
+            progress_bar.visibility = View.INVISIBLE
+            if (notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackBarMessage("Tidak ada data saat ini")
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listNotes)
+    }
+
+    private fun showSnackBarMessage(message: String) {
+        Snackbar.make(rv_notes, message, Snackbar.LENGTH_SHORT).show()
+    }
+}
---
+++ consumerapp/src/main/java/com/example/consumerapp/db/DatabaseContract.kt
@@ -0,0 +1,27 @@
+package com.example.consumerapp.db
+
+import android.net.Uri
+import android.provider.BaseColumns
+
+object DatabaseContract {
+
+    const val AUTHORITY = "com.example.mynotesapp"
+    const val SCHEME = "content"
+
+    class NoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_NAME = "note"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val DATE = "date"
+
+            //Untuk membuat URI content://com.example.mynotesapp/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
+
+        }
+    }
+}
